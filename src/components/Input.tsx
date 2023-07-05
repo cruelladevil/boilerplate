af0dd71c3b4dac19ed7fa5e7e3e6769af2da7ae3
cruelladevil <dev.timetravel@gmail.com>
2023-07-05 22:51:05 +0900

    style: 라인 수정 및 theme-color 삭제

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -2,10 +2,12 @@ import { ChangeEventHandler, useState } from 'react';
 
 const Input = () => {
   const [text, setText] = useState('');
+
   const handleText: ChangeEventHandler<HTMLInputElement> = ({ currentTarget: { value } }) => {
     setText(value);
   };
 
   return <input type="text" value={text} onChange={handleText} />;
 };
+
 export default Input;
