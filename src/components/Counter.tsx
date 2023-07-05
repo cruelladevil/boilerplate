2c9d0ae7c7a5285cd16acfeda8c8c7e701f87c39
cruelladevil <dev.timetravel@gmail.com>
2023-07-05 22:51:41 +0900

    feat: decreaseCount 추가

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -6,11 +6,15 @@ const Counter = () => {
   const increaseCount: MouseEventHandler<HTMLButtonElement> = () => {
     setCount((prev) => prev + 1);
   };
+  const decreaseCount: MouseEventHandler<HTMLButtonElement> = () => {
+    setCount((prev) => prev - 1);
+  };
 
   return (
     <div>
       <div>{count}</div>
       <button onClick={increaseCount}>더하기</button>
+      <button onClick={decreaseCount}>빼기</button>
     </div>
   );
 };
