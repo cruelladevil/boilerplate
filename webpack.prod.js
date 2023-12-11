f2fa82fb9aba70ec9a33ca5a46a10d8df77e58f4
cruelladevil <dev.timetravel@gmail.com>
2023-12-11 21:23:14 +0900

    chore: copy-webpack-plugin으로 public 폴더 복사

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,7 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
 const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = merge(common, {
   mode: 'production',
@@ -9,4 +10,17 @@ module.exports = merge(common, {
     path: path.resolve(__dirname, 'dist'),
     clean: true,
   },
+  plugins: [
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: 'public',
+          globOptions: {
+            ignore: [path.resolve(__dirname, 'public', 'index.html')],
+          },
+          noErrorOnMissing: true,
+        },
+      ],
+    }),
+  ],
 });
